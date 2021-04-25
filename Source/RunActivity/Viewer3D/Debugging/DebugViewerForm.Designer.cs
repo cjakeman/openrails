b6a1612a4e8c9e307620cb8fd68dde6cb34abbd3
Chris Jakeman <chris@tinyit.com>
2021-04-25 16:25:03 +0100

    more trials

--- Source/RunActivity/Viewer3D/Debugging/DebugViewerForm.Designer.cs
+++ Source/RunActivity/Viewer3D/Debugging/DebugViewerForm.Designer.cs
@@ -825,7 +825,6 @@
             // 
             // btnViewInGame
             // 
-            this.btnViewInGame.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnViewInGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnViewInGame.Location = new System.Drawing.Point(98, 32);
             this.btnViewInGame.Name = "btnViewInGame";
